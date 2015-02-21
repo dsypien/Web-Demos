fbe9b644348be6c7988312b2166969ef13e069b3
David Sypien <davidsypien@gmail.com>
2015-02-20 20:10:19 -0500

    Adding mongoose and mongodb

--- passport-auth/app.js
+++ passport-auth/app.js
@@ -4,10 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var expressSession = require('express-session');
 
 var routes = require('./routes/index');
 
 var app = express();
+
+// Connect to DB
+var dbConfig = require('./db.js');
+mongoose.connect(dbConfig.url);
+
+// Configure Passport
+app.use(expressSession({secret : '2k!Vpx6w9$o5pQ%M-K3Y'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configure ROUTES
 app.use('/', routes);
 
 // catch 404 and forward to error handler
