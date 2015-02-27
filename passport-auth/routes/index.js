4a7f5fa1e9ffd83a7b902b0095f0e9b876a7ae72
David Sypien <davidsypien@gmail.com>
2015-02-26 21:10:35 -0500

    Setup routes and update layout view

--- passport-auth/routes/index.js
+++ passport-auth/routes/index.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+function isAuthenticated(req, res, next){
+	// if user is authenticated in the session, call next() to get the next request handler 
+	if(req.isAuthenticated()){
+		return next();
+	}
 
-module.exports = router;
+	// if user is not authenticated then redirect to the home page
+	res.redirect('/');
+}
+
+module.exports = function(passport){
+	/* GET login page. */
+	router.get('/', function(req, res, next) {
+	  res.render('index', { message: req.flash('message') });
+	});
+
+	/* POST login info*/
+	router.post('/login', passport.authenticate('login'),{
+		successRedirect: '/home',
+		failureRedirect: '/',
+		failureFlash: true
+	})
+
+	/* GET Registraion page */
+	rouger.get('/register', function(req,res){
+		res.render('register', {message: req.flash('message')});
+	});
+
+	/* POST registration info */
+	router.post('/register', passport.authenticate('register', {
+		successRedirect: '/home',
+		failureRedirect: '/register',
+		failureFlash: true
+	}));
+
+	/* GET home page */
+	router.get('/home', isAuthenticated, function(req, res){
+		res.render('home', {user: req.user});
+	});
+
+	/* LOGOUT */
+	router.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	});
+
+	return router;
+}
